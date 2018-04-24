ad8114197b0bac895ee347ce15e8f75d2ff42ae9
Tony Yin <tonyyin89@gmail.com>
2018-04-23 18:08:11 -0700

    make bot work with coinmarketcap api

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Cryptoboto
 0. Get code: `git clone`
 1. Install dependencies: `npm install`
 2. Verify keybase installed: `which keybase`
-3. Run: `npm start`
+3. Login as your keybase bot: `keybase login`
+4. Set channel name where your bot is invited, in file `index.js`: `const CHANNEL = `
+5. Run: `npm start`
 
 ## References
 * [keybase-chat-bot](https://github.com/keybase/keybase-chat-bot)
--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-var keybaseChatBot = require('keybase-chat-bot')
+const keybaseChatBot = require('keybase-chat-bot')
+const https = require('https')
 
-var bot = new keybaseChatBot.Bot()
+
+const CHANNEL = '' // SET ME!
+
+const bot = new keybaseChatBot.Bot()
 bot.init({verbose: false}, function(err) {
   if (!err) {
-    var channel = {
-      name: 'aeto,' + bot.myInfo().username,
-      public: false,
-      topic_type: 'chat'
+    var onMessages = function(m) {
+      const channel  = m.channel
+      const messages = m.messages
+      messages.map(m => {
+        msg = m.msg.content.text.body.split(' ')
+        if (msg.length > 1) {
+          if (msg[0] === '@' + bot.myInfo().username) {
+            let cmc_raw = ''
+            const req = https.get({
+              hostname: 'api.coinmarketcap.com',
+              path: '/v1/ticker/' + msg[1].toLowerCase() + '/',
+              method: 'GET'}, res => {
+                res.on('data', doc => cmc_raw += doc)
+                res.on('end', () => {
+                  try {
+                    const cmc = JSON.parse(cmc_raw)
+                    if (cmc.length) {
+                      price = cmc[0].symbol + ' $' + Number(cmc[0].price_usd).toFixed(2) + ' (B' + cmc[0].price_btc + ')'
+                      bot.chatSend({
+                        channel: channel,
+                        message: {body: price}
+                      }, function(err, res) {
+                        if (err) console.log(err)
+                      })
+                    }
+                  } catch(err) {
+                    console.error(err.message)
+                  }
+                })
+              }
+            )
+          }
+        }
+      })
     }
-    var send_arg = {
-      channel: channel,
-      message: {
-        body: 'Hello aeto! Saying hello from my device ' + bot.myInfo().devicename
+    bot.watchChannelForNewMessages({
+      onMessages: onMessages,
+      channel: {
+        name: CHANNEL,
+        public: false,
+        topic_type: 'chat'
       }
-    }
-    bot.chatSend(send_arg, function(err) {
-      console.log('That probably sent!', err)
     })
   }
 })
